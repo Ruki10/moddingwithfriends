fa29d0c29f0be01286efda67e28d3fb8c69522db
Ruki10 <vccrowell@DESKTOP-CQU8N34>
2023-09-29 16:05:34 +0200

    umm rubens dog yummy

---
+++ src/main/java/net/mcreator/moddingwithbubenv/item/RubensDogItem.java
@@ -0,0 +1,77 @@
+
+package net.mcreator.moddingwithbubenv.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.moddingwithbubenv.ModdingwithbubenvModElements;
+
+import java.util.List;
+
+@ModdingwithbubenvModElements.ModElement.Tag
+public class RubensDogItem extends ModdingwithbubenvModElements.ModElement {
+	@ObjectHolder("moddingwithbubenv:rubens_dog")
+	public static final Item block = null;
+
+	public RubensDogItem(ModdingwithbubenvModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(19).saturation(6.8999999999999995f).setAlwaysEdible().meat().build()));
+			setRegistryName("rubens_dog");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("mm...yumme"));
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = new ItemStack(Items.BONE);
+			super.onItemUseFinish(itemstack, world, entity);
+			if (itemstack.isEmpty()) {
+				return retval;
+			} else {
+				if (entity instanceof PlayerEntity) {
+					PlayerEntity player = (PlayerEntity) entity;
+					if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
+						player.dropItem(retval, false);
+				}
+				return itemstack;
+			}
+		}
+	}
+}
